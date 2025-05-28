350ff8a3103011d801b71975ec6592c8dea12ce3
Kotasukami <75394694+Kotasukami@users.noreply.github.com>
2025-05-28 02:12:30 -0400

    Update ConfigureRegistrySettingsBatch.bat

--- ConfigureRegistrySettingsBatch.bat
+++ ConfigureRegistrySettingsBatch.bat
@@ -1,12 +1,12 @@
 @echo off
 
-set nombreNetworkProvider=NPWashuleru
+set nombreNetworkProvider=<nombre_netprov>
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%\NetworkProvider" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%\NetworkProvider" /v Class /t REG_DWORD /d 2 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%\NetworkProvider" /v Name /t REG_SZ /d %NetworkProviderName% /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%\NetworkProvider" /v ProviderPath /t REG_EXPAND_SZ /d C:\Users\Public\test_NetProv.dll /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%nombreNetworkProvider%\NetworkProvider" /v ProviderPath /t REG_EXPAND_SZ /d C:\Users\Public\<nombre_dll>.dll /f //colocar la ruta donde se ubica la DLL
 
 for /f "tokens=2*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\NetworkProvider\Order" /v ProviderOrder') do set ProviderOrder=%%B
 set NewProviderOrder=%ProviderOrder%,%nombreNetworkProvider%
