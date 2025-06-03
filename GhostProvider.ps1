74a132902cad9a92bada427f7935dca4ad946f62
Kotasukami <75394694+Kotasukami@users.noreply.github.com>
2025-06-03 03:18:01 -0400

    add ps1

---
+++ GhostProvider.ps1
@@ -0,0 +1,12 @@
+﻿   Write-Host "
+ _____ _               _  ______               _     _           
+|  __ \ |             | | | ___ \             (_)   | |          
+| |  \/ |__   ___  ___| |_| |_/ / __ _____   ___  __| | ___ _ __ 
+| | __| '_ \ / _ \/ __| __|  __/ '__/ _ \ \ / / |/ _` |/ _ \ '__|
+| |_\ \ | | | (_) \__ \ |_| |  | | | (_) \ V /| | (_| |  __/ |   
+ \____/_| |_|\___/|___/\__\_|  |_|  \___/ \_/ |_|\__,_|\___|_|" -NoNewline
+
+
+ $currentDirectory = Get-Location
+
+ Copy-Item "$currentDirectory\txt_test.txt" -Destination "C:\Users\Usuario\Desktop\PoCNetProv"
